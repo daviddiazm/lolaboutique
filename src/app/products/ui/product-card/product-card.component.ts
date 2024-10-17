64a350ead3371e5b0f63e691b419175e86e01b75
david <davidcol123dm@gmail.com>
2024-10-17 00:02:09 -0500

    card product

--- src/app/products/ui/product-card/product-card.component.ts
+++ src/app/products/ui/product-card/product-card.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
   imports: [
     CommonModule,
+    MatButtonModule
   ],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
