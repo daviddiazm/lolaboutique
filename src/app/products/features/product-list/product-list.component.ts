8bf81c1d9dd56adc688462e505de79b611b69c86
david <davidcol123dm@gmail.com>
2024-10-17 00:02:26 -0500

    list card product page

--- src/app/products/features/product-list/product-list.component.ts
+++ src/app/products/features/product-list/product-list.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ProductCardComponent } from "../../ui/product-card/product-card.component";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [
     CommonModule,
-  ],
+    ProductCardComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductListComponent { }
+export default class ProductListComponent { }
