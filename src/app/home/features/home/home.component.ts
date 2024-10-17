b6b90f878bd77d14884ab32be12a1904da8841a1
david <davidcol123dm@gmail.com>
2024-10-17 13:46:38 -0500

    Products seccion home

--- src/app/home/features/home/home.component.ts
+++ src/app/home/features/home/home.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavBarComponent } from "../../../shared/nav-bar/nav-bar.component";
+import { ProductCardComponent } from "../../../products/ui/product-card/product-card.component";
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    NavBarComponent
+    NavBarComponent,
+    ProductCardComponent,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
